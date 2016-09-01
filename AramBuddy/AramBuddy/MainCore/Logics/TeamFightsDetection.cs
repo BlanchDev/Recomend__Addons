d5e7bd0fa9c201bb6ae5873b6a9c0234ef1b4831
LeMagiclesS <emirawp@hotmail.com>
2016-09-01 07:11:03 +0300

    :100:

---
+++ AramBuddy/AramBuddy/MainCore/Logics/TeamFightsDetection.cs
@@ -0,0 +1,45 @@
+﻿using AramBuddy.MainCore.Utility;
+using EloBuddy;
+using EloBuddy.SDK;
+
+namespace AramBuddy.MainCore.Logics
+{
+    class TeamFightsDetection
+    {
+        public static void Init()
+        {
+            // Used for detecting targeted spells for TeamFights Detection
+            Obj_AI_Base.OnProcessSpellCast += delegate (Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
+            {
+                var from = sender as AIHeroClient;
+                var target = args.Target as AIHeroClient;
+                if (from != null)
+                {
+                    if (args.Slot == SpellSlot.R)
+                    {
+                        var lastAttack = new Misc.LastAttack(from, null) { Attacker = from, LastAttackSent = Core.GameTickCount, Target = null };
+                        Misc.AutoAttacks.Add(lastAttack);
+                        return;
+                    }
+                    if (target != null && from.Team != target.Team)
+                    {
+                        var lastAttack = new Misc.LastAttack(from, target) { Attacker = from, LastAttackSent = Core.GameTickCount, Target = target };
+                        Misc.AutoAttacks.Add(lastAttack);
+                    }
+                }
+            };
+
+            // Used for detecting AutoAttacks for TeamFights Detection
+            Obj_AI_Base.OnBasicAttack += delegate(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
+                {
+                    var from = sender as AIHeroClient;
+                    var target = args.Target as AIHeroClient;
+                    if (from != null && target != null)
+                    {
+                        var lastAttack = new Misc.LastAttack(from, target) { Attacker = from, LastAttackSent = Core.GameTickCount, Target = target };
+                        Misc.AutoAttacks.Add(lastAttack);
+                    }
+                };
+        }
+    }
+}
